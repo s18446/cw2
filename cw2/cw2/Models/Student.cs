23f3e7ed453d0b2bbbff48a3868dac9c2f1009c1
lekhi <danek@poczta.onet.eu>
2020-03-24 03:11:50 +0100

    testowanie notatek

---
+++ cw2/cw2/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace cw2.Models
+{
+    class Student
+    {
+        [XmlAttribute(attributeName: "email")]
+        public string Email { get; set; }
+        [XmlAttribute(attributeName: "fname")]
+        public string Imie { get; set; }
+
+        //propfull + tabx2
+
+        private string _nazwisko;
+        public string Nazwisko
+        {
+            get { return _nazwisko; }
+            set
+            {
+                if (value == null) throw new ArgumentException();
+                _nazwisko = value;
+            }
+        }
+    }
+}
---
+++ cw2/cw2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using cw2.Models;
+
+namespace cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputDestination = @"data.csv";
+            string outputDestination = @"result.xml";
+            string dataType = "xml";
+
+            if (args.Length == 3)
+            {
+                
+            }
+
+
+
+
+
+
+
+            
+        //Wczytywanie
+        var fi = new FileInfo(inputDestination);
+            using (var stream = new StreamReader(fi.OpenRead()))
+            {
+                string line = null;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    string[] kolumny = line.Split(',');
+                    Console.WriteLine(line);
+                }
+            }
+            //stream.Dispose();
+
+            //XML
+            var list = new List<Student>();
+            var st = new Student
+            {
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "kowalski@wp.pl"
+            };
+            list.Add(st);
+
+            FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>),
+                                                    new XmlRootAttribute("uczelnia"));
+            serializer.Serialize(writer, list);
+            serializer.Serialize(writer, list);
+
+
+        }
+
+    }
+}
